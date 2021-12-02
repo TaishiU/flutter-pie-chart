dfed4a8aa0067da383117ebd5dfcc224a01e7b70
TaishiU <nbadreamteam3@gmail.com>
2021-12-02 10:33:56 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late List<GDPData> _chartData;
+  late TooltipBehavior _tooltipBehavior;
+
+  @override
+  void initState() {
+    super.initState();
+    _chartData = getChartData();
+    _tooltipBehavior = TooltipBehavior(enable: true);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SfCircularChart(
+          title: ChartTitle(text: 'Continent with GDP'),
+          legend: Legend(
+            isVisible: true,
+            overflowMode: LegendItemOverflowMode.wrap,
+          ),
+          tooltipBehavior: _tooltipBehavior,
+          series: <CircularSeries>[
+            PieSeries<GDPData, String>(
+              dataSource: _chartData,
+              xValueMapper: (GDPData data, _) => data.continent,
+              yValueMapper: (GDPData data, _) => data.gdp,
+              dataLabelSettings: DataLabelSettings(
+                isVisible: true,
+              ),
+              enableTooltip: true,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<GDPData> getChartData() {
+    final List<GDPData> chartData = [
+      GDPData('Oceania', 1600),
+      GDPData('Africa', 2490),
+      GDPData('S America', 2900),
+      GDPData('Europe', 23050),
+      GDPData('N America', 24800),
+      GDPData('Asia', 34390),
+    ];
+    return chartData;
+  }
+}
+
+class GDPData {
+  final String continent;
+  final int gdp;
+
+  GDPData(this.continent, this.gdp);
+}
